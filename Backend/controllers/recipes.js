09087c8202d99d5e45e692fd409b89ae02823b5e
Yash ved <yashved01@gmail.com>
2023-10-12 15:16:25 +0530

    update

--- Backend/controllers/recipes.js
+++ Backend/controllers/recipes.js
@@ -33,8 +33,8 @@ exports.addRecipe =  async (req,res) => {
 exports.addToCollection = async (req, res) => {
     try {
   //     const { cookies } = req;
-  const userId = req.params.userId;
-      const recipeId  = req.params.recipeId;
+  const userId = req.body.userId;
+      const recipeId  = req.body.recipeId;
 
   console.log(userId);
   // console.log(recipeId);
